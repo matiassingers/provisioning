1f0c1c14906a11d3cfe78c3c51f3138d6275f552
Matias Singers <mail@matiassingers.com>
2015-01-24 00:13:54 +0800

    Add index.js logic

--- index.js
+++ index.js
@@ -1,5 +1,14 @@
 'use strict';
 
-module.exports = function(){
+var exec = require('child_process').exec;
+var plist = require('simple-plist');
 
+module.exports = function(file, callback){
+  exec('security cms -D -i ' + file, function(error, output){
+    if(error){
+      return callback(error);
+    }
+
+    callback(null, plist.parse(output));
+  });
 };
